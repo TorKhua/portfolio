0f3b7714207b7762ad340a8d328704d904ccef3a
TorKhua <alejandrotorcuatomorgado@gmail.com>
2024-02-13 23:12:23 +0100

    add and test projects

---
+++ src/components/RoundedTag.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+function RoundedTag(props : {icon ?: ReactNode}) {
+  return (
+    <div className='flex items-center align-middle justify-center p-1 '>
+      {props.icon}
+    </div>
+  )
+}
+
+export default RoundedTag
--- src/components/Sections/Project/Projects.tsx
+++ src/components/Sections/Project/Projects.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Section from '../Section'
+import ProjectCard from '@/components/ProjectCard'
 
 function Projects() {
   return (
     <Section title='Proyectos'>
-      
+      <div className='flex flex-wrap gap-6'>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      </div>
     </Section>
   )
 }
